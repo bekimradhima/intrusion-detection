3a326f6473eea74c0f582853ef6da1f2415a6860
bekimradhima <bekimradhima@gmail.com>
2021-04-12 14:30:48 +0200

    modifiche
    ho aggiunto un filtro gaussiano prima del calcolo della mask, implementato un selective background update con due criteri: il calcolo di una mask tra nuovo bg usando alpha blending e vecchio bg, nel caso il cambiamento sia quasi zero fa update del bg (in quanto a video ho il vero background), e nel caso di repentini aumenti di luce.
    Il criterio per valutare i cambiamenti e' semplicemente mean quindi si puo cambiare per dare piu robustezza.
    Per ora funzionicchia anche abbassando il threshold a 20
    Infine ho implementato il countour detection.

---
+++ selective_background_otsu_2frame.py
@@ -0,0 +1,224 @@
+import numpy as np
+import cv2
+import time
+
+
+def L1(img1, img2):
+    diff = np.abs(img1 - img2)
+    print(len(img1.shape))
+    if img1.shape[-1] == 3 and len(img1.shape) == 3:
+        diff = np.sum(diff, axis=-1)
+    return diff
+
+
+def L2(img1, img2):
+    sq_dist = (img1 - img2) ** 2
+    if img1.shape[-1] == 3 and len(img1.shape) == 3:
+        sq_dist = np.sum(sq_dist, axis=-1)
+    diff = np.sqrt(sq_dist)
+    return diff
+
+
+def Linf(F1, F2):
+    diff = np.abs(F1 - F2)
+    if F1.shape[-1] == 3 and len(F1.shape) == 3:
+        diff = np.max(diff, axis=-1)
+    return diff
+
+
+def twoframedifference(frame, previousframe, distance_type, threshold):
+    distance = distance_type(frame, previousframe)
+    maskbool = distance > threshold
+    mask = maskbool.astype(np.uint8) * 255
+    return [mask]
+
+
+def pfm(hist):
+    total_pixel = np.sum(hist)
+    pfm = []
+    for i in range(256):
+        pfm_i = np.sum(hist[:i]) / total_pixel
+        pfm.append(pfm_i)
+    return np.asarray(pfm)
+
+
+Rectangular_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
+# Defining a variable interpolation for mean or median functions
+interpolation = np.median  # or np.mean
+alfa = 0.2
+
+
+def background_initialization(bg, n, cap, count):
+    while cap.isOpened() and count < n:
+        ret, frame = cap.read()
+        frame=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+        if ret or not frame is None:
+            # Release the Video if ret is false
+            # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            # hist,bins = np.histogram(frame.flatten(),256,[0,256])
+            # eq_op = pfm(hist)*255
+            # frame = eq_op[frame]
+            frame
+            if count == 0:
+                bg.append(frame)
+            else:
+                bg.append(alfa * frame)
+                bg[count] = (1 - alfa) * bg[count - 1] + bg[count]
+            count += 1
+            # print(count)
+        else:
+            break
+    cap.release()
+    bg_inter = np.stack(bg, axis=0)
+    bg_inter = interpolation(bg_inter, axis=0)
+    cv2.destroyAllWindows()
+    return [bg_inter, count]
+
+
+def background_update(bg, prev_bg):
+    bg = (1 - alfa) * prev_bg + alfa * bg
+    return bg
+
+
+###Define change detection parameters
+thr = 20
+distance = L2
+bg = []
+N_frames = 40 # then refresh
+
+
+# def check_light():
+
+def sobel(img):
+    dst1 = cv2.Sobel(img, -1, 1, 0, 3)
+    dst2 = cv2.Sobel(img, -1, 0, 1, 3)
+    sob = np.maximum(dst1, dst2)
+    return sob
+
+
+# blob detector parameters
+
+personDetectorParameters = cv2.SimpleBlobDetector_Params()
+bookDetectorParameters = cv2.SimpleBlobDetector_Params()
+
+# define params for person detection
+personDetectorParameters.filterByArea = True
+personDetectorParameters.minArea = 5000  # 5000
+personDetectorParameters.maxArea = 100000
+personDetectorParameters.minDistBetweenBlobs = 0
+personDetectorParameters.filterByCircularity = False
+personDetectorParameters.filterByColor = True
+personDetectorParameters.blobColor = 255
+personDetectorParameters.filterByConvexity = False
+personDetectorParameters.filterByInertia = False
+
+# define params for book detection
+bookDetectorParameters.filterByArea = True
+bookDetectorParameters.minArea = 1000  # 1000
+bookDetectorParameters.maxArea = 5000  # 5000
+bookDetectorParameters.minDistBetweenBlobs = 0
+bookDetectorParameters.filterByCircularity = False
+bookDetectorParameters.filterByColor = True
+bookDetectorParameters.blobColor = 255
+bookDetectorParameters.filterByConvexity = False
+bookDetectorParameters.filterByInertia = False
+
+detector_person = cv2.SimpleBlobDetector_create(personDetectorParameters)
+detector_book = cv2.SimpleBlobDetector_create(bookDetectorParameters)
+cap = cv2.VideoCapture('1.avi')
+count = 0
+idx = 0
+# computation of the background
+[bg, count] = background_initialization(bg, N_frames, cap, count)
+oldbg=bg
+
+
+# bg=cv2.cvtColor(bg.astype(np.uint8), cv2.COLOR_BGR2GRAY)
+
+def change_detection(video_path, bg, threshold, idx):
+    # previous_frames = []
+    cap = cv2.VideoCapture(video_path)
+    update = 0
+    while (cap.isOpened()):
+        # Capture frame
+        ret, frame = cap.read()
+        if not ret or frame is None:
+            # Release the Video if ret is false
+            cap.release()
+            print("Released Video Resource")
+            # Break exit the for loops
+            break
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        gray = cv2.GaussianBlur(gray, (5, 5), 0)
+
+
+        mask = (distance(gray, bg) > threshold)
+        mask = mask.astype(np.uint8) * 255
+        blur = cv2.GaussianBlur(mask, (5, 5), 0)
+        eroded = cv2.erode(blur, None, iterations=3)
+        # ret, thresh = cv2.threshold(blur, 100, 255, 0) no difference in the video with otsu but i like japanese
+        ret, thresh = cv2.threshold(eroded, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+        cv2.imshow('thresh', thresh)
+        dilated = cv2.dilate(thresh, Rectangular_kernel, iterations=3)
+        closed = cv2.morphologyEx(dilated, cv2.MORPH_CLOSE, Rectangular_kernel)
+        cv2.imshow('closed', closed)
+
+
+
+        #compute the difference with background using alpha blending
+        bgg = bg.astype(np.uint8)
+        bgg1 = bg.astype(np.uint8)
+        bgg[np.logical_not(closed)] = np.asarray([-255])
+        closed1 = 255 - closed
+        bgg1[np.logical_not(closed1)] = np.asarray([255])
+        bgc = bgg + bgg1
+        tbg = background_update(bgc, bg)
+        # bgc=bgg+gray1
+        cv2.imshow('background', tbg.astype(np.uint8))
+
+
+
+
+        bgmask = distance(alfa*gray+(1-alfa)*bg,bg) > 20
+        bgmask = bgmask.astype(np.uint8) * 255
+        cv2.imshow('bgmask',bgmask)
+        #print(np.mean(bgmask))
+
+        #selective update when no change detected with background
+        if np.mean(bgmask) < 0.5 :
+            bg = background_update(gray, bg)
+            print('selective update')
+
+        #update background when ligth changes
+        if np.mean(closed) > 110 :
+            bg = background_update(gray, bg)
+
+            print('change_updated')
+
+
+        keypoints = detector_person.detect(eroded)
+        im_keypoints = cv2.drawKeypoints(frame, keypoints, np.array([]), (0, 0, 255),
+                                         cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+        keypoints2 = detector_book.detect(closed)
+        im_keypoints2 = cv2.drawKeypoints(im_keypoints, keypoints2, np.array([]), (255, 0, 0),
+                                          cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+        cv2.imshow('Video', im_keypoints2)
+        _, contours, hierarchy = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        for i, cnt in enumerate(contours):
+            # if the size of the contour is greater than a threshold
+            if cv2.contourArea(cnt) > 5000:
+                cv2.drawContours(frame, [cnt], 0, (0, 255, 0), 5)  # if >0 shows contour
+            elif cv2.contourArea(cnt) > 1000:
+                cv2.drawContours(frame, [cnt], 0, (0, 0, 255), 5)  # if >0 shows contour
+        cv2.imshow('contours', frame)
+
+        time.sleep(0.02)
+        idx += 1
+        if cv2.waitKey(1) == ord('q'):
+            break
+    print("Released Video Resource")
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+change_detection('1.avi', bg, thr, idx)
